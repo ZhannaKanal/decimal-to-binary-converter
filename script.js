4c6b85c3e56c11f38c4ca8e512ae0fc87b4d0d9a
Zhanna <kezhanna.03@mail.ru>
2025-11-24 16:39:26 +0500

    tasks 55-61

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-const callStack = [
-  'a(): returns "freeCodeCamp " + "is awesome!"',
-];
-
-const a = () => {
-  return "freeCodeCamp " + b();
-};
-
-const b = () => {
-  return "is " + c();
-};
-
-const c = () => {
-  return "awesome!";
-};
-
-console.log(a());
-
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+const countdown = (number) => {
+  console.log(number);
+
+  if (number === 0) {
+    return;
+  } else {
+    countdown(number - 1);
+  }
+};
+
+countdown(3);
 
 const decimalToBinary = (input) => {
   let binary = "";
